075ce2d5f46697e4e94a0003d5d6b41882693347
fdev31 <fdev31@gmail.com>
2023-12-03 16:23:39 +0100

    sendCode: mouse support

--- scripts/sendCode.py
+++ scripts/sendCode.py
@@ -12,17 +12,50 @@ AZERTY = True
 
 ser = serial.Serial(port=sys.argv[1], baudrate=9600)
 
+HEADER = [0x57, 0xAB, 0x00]
+
+SCREEN_RES_X = 4096
+SCREEN_RES_Y = 4096
+
+
+def HID(values):
+    packet = HEADER + values
+    packet.append(sum(packet) % 256)
+    return packet
+
+
+def genMouse(x=0, y=0, left=False, right=False, middle=False, scroll=0, release=False):
+    buttons = 0
+    if left:
+        buttons |= 1
+    if right:
+        buttons |= 2
+    if middle:
+        buttons |= 4
+    if x < 0:
+        x += 255
+    if y < 0:
+        y += 255
+    if scroll:
+        scroll = 127 + scroll
+
+    press = HID([0x05, 0x05, 0x01, buttons, x, y, scroll])
+    ser.write(press)
+
+    if release:
+        time.sleep(0.02)
+        release = HID([0x05, 0x05, 0x00, 0x0, 0x00, 0x00, 0x0])
+        ser.write(release)
+
 
 def genKey(
     key=0x04,
     modifiers=0,
 ):
-    press = [0x57, 0xAB, 0x00, 0x02, 0x08, modifiers, 0x00, key, 0x00, 0x00, 0x00, 0x00, 0x00]
-    press.append(sum(press) % 256)
+    press = HID([0x02, 0x08, modifiers, 0x00, key, 0x00, 0x00, 0x00, 0x00, 0x00])
     ser.write(press)
     time.sleep(0.02)
-    rel = [0x57, 0xAB, 0x00, 0x02, 0x08, modifiers, 0x00, 0x0, 0x00, 0x00, 0x00, 0x00, 0x00]
-    rel.append(sum(rel) % 256)
+    rel = HID([0x02, 0x08, modifiers, 0x00, 0x0, 0x00, 0x00, 0x00, 0x00, 0x00])
     ser.write(rel)
 
 
@@ -56,21 +89,34 @@ def typeKey(n):
     time.sleep(0.1)
 
 
-typeKey(100)
-time.sleep(0.05)
-# Normal
-for n in range(4, 57):
-    if 40 <= n <= 44:
-        continue
-    # 40=RETURN
-    # 41=DEL
-    # 42=BS
-    # 43=TAB
-    # 44=SPACE
-    typeKey(n)
+def main():
+    typeKey(100)
     time.sleep(0.05)
+    # Normal
+    for n in range(4, 57):
+        if 40 <= n <= 44:
+            continue
+        # 40=RETURN
+        # 41=DEL
+        # 42=BS
+        # 43=TAB
+        # 44=SPACE
+        typeKey(n)
+        time.sleep(0.05)
 
-# 4 operators
-for n in range(84, 88):
-    typeKey(n)
-    time.sleep(0.05)
+    # 4 operators
+    for n in range(84, 88):
+        typeKey(n)
+        time.sleep(0.05)
+
+
+def jiggle_mouse():
+    while True:
+        genMouse(x=1)
+        time.sleep(0.1)
+        genMouse(x=-1)
+        time.sleep(1)
+
+
+main()
+jiggle_mouse()
