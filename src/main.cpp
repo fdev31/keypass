a32a03ef1ad39151db33f204cb08b31b43860796
fdev31 <fdev31@gmail.com>
2025-05-18 21:08:01 +0200

    add auto shutdown

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,23 @@
 #include <Arduino.h>
 
 const int baudRate = 9600;
+const unsigned long SHUTDOWN_TIMEOUT = 5 * 60 * 1000; // 5 minutes in ms
+unsigned long startTime;
 
 void setup() {
   Serial.begin(baudRate);
   captiveSetup();
   setUpKeyboard(server);
+  startTime = millis();
 }
 
 void loop() {
   captiveLoop();
-  delay(100);
+
+  // Check if it's time to go to deep sleep
+  if (millis() - startTime >= SHUTDOWN_TIMEOUT) {
+    ESP.deepSleep(0); // Deep sleep indefinitely
+  }
+
+  delay(200);
 }
