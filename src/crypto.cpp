bc313adc4bd707123382d0f3c314a2fe667fca4d
fdev31 <fdev31@gmail.com>
2025-07-02 00:00:33 +0200

    update note

--- src/crypto.cpp
+++ src/crypto.cpp
@@ -1,5 +1,5 @@
 // NOTE: uses a single buffer, can only handle one at a time !
-// Consider using BLACKE2 instead of XX
+// Consider using BLACKE2 instead of XX and CHACHA instead of SPECK
 #include "crypto.h"
 #include "constants.h"
 Speck speck;
