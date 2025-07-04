92653c8370d1896410fa3c6c82f0680f8579c5b3
fdev31 <fdev31@gmail.com>
2025-07-05 00:52:04 +0200

    more BLE

--- src/bluetooth.cpp
+++ src/bluetooth.cpp
@@ -1,8 +1,10 @@
+#include "configuration.h"
+#if ENABLE_BLUETOOTH
 #include "bluetooth.h"
 #include "password.h"
 #include <NimBLEDevice.h>
 
-#if ENABLE_BLUETOOTH
+extern void ping();
 
 #define SERVICE_UUID "12345678-1234-1234-1234-123456789abc"
 #define LIST_CHAR_UUID "12345678-1234-1234-1234-123456789abd"
@@ -12,6 +14,7 @@
 
 class ListCallback : public NimBLECharacteristicCallbacks {
   void onRead(NimBLECharacteristic *pCharacteristic) {
+    ping();
     String passwords = listPasswords();
     pCharacteristic->setValue(passwords.c_str());
   }
@@ -19,14 +22,17 @@ class ListCallback : public NimBLECharacteristicCallbacks {
 
 class FetchCallback : public NimBLECharacteristicCallbacks {
   void onWrite(NimBLECharacteristic *pCharacteristic) {
+    ping();
     std::string value = pCharacteristic->getValue();
     if (value.length() > 0) {
       int id = atoi(value.c_str());
       const char *password = fetchPassword(id);
       if (password) {
         pCharacteristic->setValue(password);
+        pCharacteristic->notify();
       } else {
         pCharacteristic->setValue("Invalid ID");
+        pCharacteristic->notify();
       }
     }
   }
@@ -34,6 +40,7 @@ class FetchCallback : public NimBLECharacteristicCallbacks {
 
 class EditCallback : public NimBLECharacteristicCallbacks {
   void onWrite(NimBLECharacteristic *pCharacteristic) {
+    ping();
     std::string value = pCharacteristic->getValue();
     // Expecting "id,name,password,layout"
     // Simplified parsing, assuming well-formed input
@@ -43,19 +50,26 @@ class EditCallback : public NimBLECharacteristicCallbacks {
     int layout = atoi(strtok(NULL, ","));
     if (editPassword(id, name, password, layout)) {
       pCharacteristic->setValue("OK");
+      pCharacteristic->notify();
     } else {
       pCharacteristic->setValue("Error");
+      pCharacteristic->notify();
     }
   }
 };
 
 class TypeCallback : public NimBLECharacteristicCallbacks {
   void onWrite(NimBLECharacteristic *pCharacteristic) {
+    ping();
     std::string value = pCharacteristic->getValue();
     if (value.length() > 0) {
       int id = atoi(value.c_str());
       if (typePassword(id, -1, true)) {
-        // Cannot provide feedback here as it's a write characteristic
+        pCharacteristic->setValue("OK");
+        pCharacteristic->notify();
+      } else {
+        pCharacteristic->setValue("Error");
+        pCharacteristic->notify();
       }
     }
   }
@@ -70,31 +84,39 @@ void bluetoothSetup() {
   BLECharacteristic *pListChar = pService->createCharacteristic(
       LIST_CHAR_UUID, NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::NOTIFY);
   pListChar->setCallbacks(new ListCallback());
+  pListChar->createDescriptor("2901", NIMBLE_PROPERTY::READ)
+      ->setValue("List Passwords");
 
   // Fetch Password
-  BLECharacteristic *pFetchChar =
-      pService->createCharacteristic(FETCH_CHAR_UUID, NIMBLE_PROPERTY::WRITE);
+  BLECharacteristic *pFetchChar = pService->createCharacteristic(
+      FETCH_CHAR_UUID, NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::NOTIFY);
   pFetchChar->setCallbacks(new FetchCallback());
+  pFetchChar->createDescriptor("2901", NIMBLE_PROPERTY::READ)
+      ->setValue("Fetch Password by ID");
 
   // Edit Password
-  BLECharacteristic *pEditChar =
-      pService->createCharacteristic(EDIT_CHAR_UUID, NIMBLE_PROPERTY::WRITE);
+  BLECharacteristic *pEditChar = pService->createCharacteristic(
+      EDIT_CHAR_UUID, NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::NOTIFY);
   pEditChar->setCallbacks(new EditCallback());
+  pEditChar->createDescriptor("2901", NIMBLE_PROPERTY::READ)
+      ->setValue("Edit Password by ID");
 
   // Type Password
-  BLECharacteristic *pTypeChar =
-      pService->createCharacteristic(TYPE_CHAR_UUID, NIMBLE_PROPERTY::WRITE);
+  BLECharacteristic *pTypeChar = pService->createCharacteristic(
+      TYPE_CHAR_UUID, NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::NOTIFY);
   pTypeChar->setCallbacks(new TypeCallback());
+  pTypeChar->createDescriptor("2901", NIMBLE_PROPERTY::READ)
+      ->setValue("Type Password by ID");
 
   pService->start();
 
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  
+
   BLEAdvertisementData advertisementData = BLEAdvertisementData();
   advertisementData.setName("KeyPass");
   advertisementData.setFlags(0x06); // GENERAL_DISC_MODE | BR_EDR_NOT_SUPPORTED
   advertisementData.addServiceUUID(SERVICE_UUID);
-  
+
   pAdvertising->setAdvertisementData(advertisementData);
   pAdvertising->start();
 }
