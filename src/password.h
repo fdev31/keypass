238f2cfab4d81cb7dbcd8de6835ccfca846dce44
fdev31 <fdev31@gmail.com>
2025-07-09 20:00:37 +0200

    fix type of layout property

--- src/password.h
+++ src/password.h
@@ -8,7 +8,7 @@
 
 typedef struct {
   char name[MAX_NAME_LEN];
-  uint8_t layout;
+  int8_t layout;
   byte password[MAX_PASS_LEN];
   // NOTE: following are not defined since they are not used in property API
   // uint8_t pass_len;
