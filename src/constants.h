23d057f46537f1cdb5e2d7eaf9cafe63b1b700ab
fdev31 <fdev31@gmail.com>
2025-07-13 16:30:24 +0200

    leave the version out of the dumped string

--- src/constants.h
+++ src/constants.h
@@ -10,7 +10,7 @@
 #define STO_BLOCK_SIZE 16
 #define RAND_BYTE_FACTOR RAND_MAX / 255
 
-#define CRYPTO_HEADER_SIZE 3 // salt + version + layout
+#define CRYPTO_HEADER_SIZE 2 // salt + layout
 
 #define META_SIZE 32
 #define MAX_NAME_LEN META_SIZE - CRYPTO_HEADER_SIZE
@@ -18,7 +18,10 @@
 
 #define CRYPTO_OVERHEAD 0
 
+// Encrypted dump length
 #define DUMP_LENGTH (META_SIZE + MAX_PASS_LEN + CRYPTO_OVERHEAD)
+static_assert(DUMP_LENGTH % STO_BLOCK_SIZE == 0,
+              "DUMP_LENGTH must be a multiple of STO_BLOCK_SIZE");
 
 // Preferences fields
 #define F_NAMESPACE "KeyPass" // used for global settings
--- src/importexport.cpp
+++ src/importexport.cpp
@@ -14,55 +14,50 @@ String dumpSinglePassword(const char *label, const char *password,
                           int password_size, const char layout,
                           const unsigned char version, int index) {
 
-  char buffer[DUMP_LENGTH];
+  char buffer[DUMP_LENGTH + 1];
 
   char salt = random() % 255; // Generate a random salt byte
   char *ptr = buffer;
+  *ptr++ = version;
   *ptr++ = salt;
-  *ptr++ = salt ^ version;
   *ptr++ = salt ^ layout;
 
-  int sz = strlen(label) + 1;
-  memcpy(ptr, label, sz);
+  int labelLength = strlen(label) + 1;
+  memcpy(ptr, label, labelLength);
   ptr += MAX_NAME_LEN;
 
-  randomizeBuffer((uint8_t *)buffer + CRYPTO_HEADER_SIZE + sz,
-                  DUMP_LENGTH - CRYPTO_HEADER_SIZE - sz);
+  randomizeBuffer((uint8_t *)buffer + CRYPTO_HEADER_SIZE + labelLength + 1,
+                  DUMP_LENGTH - CRYPTO_HEADER_SIZE - labelLength - 1);
 
   memcpy(ptr, password, MAX_PASS_LEN);
 
   // fprintf(stderr, "DEBUG=>%s#\n",
   //         hexDump((const uint8_t *)buffer, DUMP_LENGTH).c_str());
 
-  encryptBuffer((const char *)buffer, (uint8_t *)buffer, index, DUMP_LENGTH);
+  encryptBuffer(buffer + 1, (uint8_t *)buffer + 1, index, DUMP_LENGTH);
 
-  return hexDump((const uint8_t *)buffer, DUMP_LENGTH);
+  return hexDump((const uint8_t *)buffer, DUMP_LENGTH + 1);
 }
 
 bool parseSinglePassword(const char *rawdata, char *label, char *password,
                          int *password_size, char *layout,
                          unsigned char *version, int index) {
 
-  // Calculate the binary data size needed
-  uint8_t buffer[DUMP_LENGTH];
+  uint8_t buffer[DUMP_LENGTH + 1];
+  uint8_t *ptr = buffer;
 
-  // Convert hex dump to binary
-  hexParse(rawdata, buffer, DUMP_LENGTH);
+  hexParse(rawdata, buffer, DUMP_LENGTH + 1);
+  ptr++;
 
-  decryptBuffer((const uint8_t *)buffer, (char *)buffer, index, DUMP_LENGTH);
+  decryptBuffer(ptr, (char *)ptr, index, DUMP_LENGTH);
+  *version = buffer[0];
+  *layout = ptr[1] ^ ptr[0];
+  ptr += CRYPTO_HEADER_SIZE;
 
-  char salt = buffer[0];
+  strncpy(label, (const char *)ptr, MAX_NAME_LEN);
+  ptr += MAX_NAME_LEN;
 
-  *version = buffer[1] ^ salt;
-  *layout = buffer[2] ^ salt;
-
-  // Decrypt the data starting after version and layout
-
-  // Extract label (name)
-  strncpy(label, (const char *)buffer + CRYPTO_HEADER_SIZE, MAX_NAME_LEN);
-
-  // Extract password (which follows the label)
-  memcpy(password, (const char *)buffer + META_SIZE, MAX_PASS_LEN);
+  memcpy(password, (const char *)ptr, MAX_PASS_LEN);
 
   return true;
 }
